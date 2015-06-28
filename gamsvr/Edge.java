21108099a6ac8266526cdca3000b8c4afc169564
OksanaMykhalets <oksanamykhalets@gmail.com>
2015-06-29 02:52:57 +0300

    Create Edge.java

---
+++ gamsvr/Edge.java
@@ -0,0 +1,33 @@
+public class Edge {
+	
+	private Vertex startVertex;
+	
+	private Vertex endVertex;
+	
+	private int weight;
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public Vertex getStartVertex() {
+		return startVertex;
+	}
+
+	public void setStartVertex(Vertex startVertex) {
+		this.startVertex = startVertex;
+	}
+
+	public Vertex getEndVertex() {
+		return endVertex;
+	}
+
+	public void setEndVertex(Vertex endVertex) {
+		this.endVertex = endVertex;
+	}
+
+}
