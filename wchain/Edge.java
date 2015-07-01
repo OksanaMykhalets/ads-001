b6056130e92508a471f9e7fedb0dde5254c1ca92
OksanaMykhalets <oksanamykhalets@gmail.com>
2015-07-01 16:07:28 +0300

    Create Edge.java

---
+++ wchain/Edge.java
@@ -0,0 +1,23 @@
+public class Edge {
+	
+	private Vertex startVertex;
+	
+	private Vertex endVertex;
+
+	public Vertex getStartVertex() {
+		return startVertex;
+	}
+
+	public void setStartVertex(Vertex startVertex) {
+		this.startVertex = startVertex;
+	}
+
+	public Vertex getEndVertex() {
+		return endVertex;
+	}
+
+	public void setEndVertex(Vertex endVertex) {
+		this.endVertex = endVertex;
+	}
+
+}
