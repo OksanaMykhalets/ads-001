48de6c67c8e314539c683e8d97a25c7c5f30f20d
OksanaMykhalets <oksanamykhalets@gmail.com>
2015-07-18 11:53:55 +0300

    Update Graph.java

--- wchain/Graph.java
+++ wchain/Graph.java
@@ -1,27 +1,26 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Graph {
 
-	private ArrayList<HashMap<String, Vertex>> vertices;
+	private ArrayList<ArrayList<Vertex>> vertices;
 
 	private List<Edge> edges;
 
 	public Graph() {
-		this.vertices = new ArrayList<HashMap<String, Vertex>>();
-		for (int i=0; i<=50; i++) {
-			this.vertices.add(new HashMap<String, Vertex>());
+		this.vertices = new ArrayList<ArrayList<Vertex>>();
+		for (int i = 0; i <= 50; i++) {
+			this.vertices.add(new ArrayList<Vertex>());
 		}
 		this.edges = new LinkedList<Edge>();
 	}
 
-	public ArrayList<HashMap<String, Vertex>> getVertices() {
+	public ArrayList<ArrayList<Vertex>> getVertices() {
 		return vertices;
 	}
 
-	public void setVertices(ArrayList<HashMap<String, Vertex>> vertices) {
+	public void setVertices(ArrayList<ArrayList<Vertex>> vertices) {
 		this.vertices = vertices;
 	}
 
