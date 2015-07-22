cf85ead41e407cc593d090920c51135e7e1f63b4
OksanaMykhalets <oksanamykhalets@gmail.com>
2015-07-22 13:52:28 +0300

    optimized IJONES (works for W,N <= 1000)

--- ijones/Main.java
+++ ijones/Main.java
@@ -4,15 +4,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
+	public static final int LOWERCASE_A = 97;
+	public static final int LOWERCASE_Z = 122;
 	public static int passageWidth;
 	public static int passageHeight;
 	public static char[][] tiles;
 	public static BigInteger numberOfPossiblePaths;
-	public static HashMap<Character, LinkedList<int[]>> charactersCoordinates;
+	public static HashMap<Character, BigInteger> allPossiblePathsForPreviousColumns;
 
 	public static void main(String[] args) {
 		readDataFromFile();
@@ -21,10 +22,6 @@ public class Main {
 	}
 
 	public static void readDataFromFile() {
-		charactersCoordinates = new HashMap<>();
-		for (int i = 97; i <= 122; i++) {
-			charactersCoordinates.put((char) i, new LinkedList<int[]>());
-		}
 		try {
 			Scanner scanner = new Scanner(new File("ijones.in"));
 			passageWidth = scanner.nextInt();
@@ -34,10 +31,6 @@ public class Main {
 				String row = scanner.next();
 				for (int j = 0; j < passageWidth; j++) {
 					tiles[i][j] = row.charAt(j);
-					int[] charCoordinates = new int[2];
-					charCoordinates[0] = i;
-					charCoordinates[1] = j;
-					charactersCoordinates.get(tiles[i][j]).add(charCoordinates);
 				}
 			}
 			scanner.close();
@@ -48,32 +41,41 @@ public class Main {
 	}
 
 	public static void calculateNumberOfPossiblePaths() {
+		allPossiblePathsForPreviousColumns = new HashMap<Character, BigInteger>();
+		for (int i = LOWERCASE_A; i <= LOWERCASE_Z; i++) {
+			allPossiblePathsForPreviousColumns.put((char) i,
+					new BigInteger("0"));
+		}
 		BigInteger[][] distances = new BigInteger[passageHeight][passageWidth];
 		for (int i = 0; i < passageHeight; i++) {
 			for (int j = 0; j < passageWidth; j++) {
 				if (j == 0) {
-					distances[i][j] = BigInteger.ONE;
+					distances[i][j] = new BigInteger("1");
+					BigInteger newPath = allPossiblePathsForPreviousColumns
+							.get(tiles[i][j]).add(BigInteger.ONE);
+					allPossiblePathsForPreviousColumns
+							.put(tiles[i][j], newPath);
 				} else {
-					distances[i][j] = BigInteger.ZERO;
+					distances[i][j] = new BigInteger("0");
 				}
 			}
 		}
+
 		for (int column = 1; column < passageWidth; column++) {
 			for (int row = 0; row < passageHeight; row++) {
 				char currentTile = tiles[row][column];
 				if (currentTile != tiles[row][column - 1]) {
 					distances[row][column] = distances[row][column - 1];
 				}
-				LinkedList<int[]> allTilesWithSomeLetter = charactersCoordinates
-						.get(currentTile);
-				for (int[] coordinates : allTilesWithSomeLetter) {
-					int tileRow = coordinates[0];
-					int tileColumn = coordinates[1];
-					if (tileColumn < column) {
-						distances[row][column] = distances[row][column]
-								.add(distances[tileRow][tileColumn]);
-					}
-				}
+				distances[row][column] = distances[row][column]
+						.add(allPossiblePathsForPreviousColumns
+								.get(currentTile));
+			}
+			for (int row = 0; row < passageHeight; row++) {
+				char currentTile = tiles[row][column];
+				BigInteger newPath = allPossiblePathsForPreviousColumns.get(
+						currentTile).add(distances[row][column]);
+				allPossiblePathsForPreviousColumns.put(currentTile, newPath);
 			}
 		}
 		numberOfPossiblePaths = distances[0][passageWidth - 1];
